e12977f3885da093f59eea97d1515161a040ce9e
Steven Chiang <Stevenchiang57@csu.fullerton.edu>
2020-02-06 03:40:39 -0800

    modified fold

--- fold.c
+++ fold.c
@@ -4,27 +4,35 @@
 #include "files.h"
 
 #define MAXNUM 300
+#define MAX_TEXT_PER_LINE 10
 int main(int argc, const char * argv[]) {
   FILE* fin;
   FILE* fout;
 
+  if (!open_io_files(argc, argv, &fin, &fout, 3, 4,                   "Usage: ./detab inputfile outputfile tab_interval (optional)\n")) {
+    exit(1);
+  }
+
   int c, chars, lastblank;
   char fold[MAXNUM];
+  chars = lastblank = 0;
   while ((c = fgetc(fin)) != EOF){
     fold[chars] = c;
-    ++chars;
-    if (fold[chars - 1] == ' ' || fold[chars - 1] == '\t'){
-      lastblank = chars - 1;
+    if (fold[chars] == ' ' || fold[chars] == '\t'){
+      lastblank = chars;
       }
-    if (chars < 10){
+    ++chars;
+    if (chars > MAX_TEXT_PER_LINE){
       fold[lastblank] = '\n';
-      for(int i = 0; i <= lastblank; ++i){
+      for(int i = 0; i < MAX_TEXT_PER_LINE + 1; ++i){
         fputc(fold[i], fout);
       }
       chars = 0;
     }
+
   }
 
   closefiles(2, fin, fout);  // must say number of files
 
   return 0;
+}
