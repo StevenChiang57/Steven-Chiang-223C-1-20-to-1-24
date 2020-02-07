aaf50725e018a2b15a45954a779822d92d52bea7
Steven Chiang <Stevenchiang57@csu.fullerton.edu>
2020-02-06 19:16:07 -0800

    modified comment

--- comment.c
+++ comment.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "files.h"
 
-#define MAXNUM 300
+#define MAXNNUM 500
 #define IN 1
 #define OUT 0
 int main(int argc, const char * argv[]) {
@@ -13,32 +13,51 @@ int main(int argc, const char * argv[]) {
   if (!open_io_files(argc, argv, &fin, &fout, 3, 4,                   "Usage: ./detab inputfile outputfile tab_interval (optional)\n")) {
     exit(1);
   }
-  /* */
-  int c, incomment, inquotes, lastchar = fgetc(fin);
-  char noncomment[MAXNUM];
-  while ((c = fgetc(fin)) != EOF){
-    if (lastchar == '"'){
+
+  int lastchar, c , nextchar;
+  while ((c = fgetc(fin)) != EOF) {
+    if (c == '"'){
+      lastchar = c;
+      fputc(c , fout);
+      c = fgetc(fin);
       while (c != '"'){
-        fputc(lastchar, fout);
         lastchar = c;
+        fputc(lastchar, fout);
         c = fgetc(fin);
+      }
+      lastchar = c;
+      fputc(lastchar, fout);
+    }
+    else if (c == '/'){
+      nextchar = fgetc(fin);
+      if(nextchar == '*'){  // /* comment */ case
+        while (lastchar != '*' || c != '/'){
+          lastchar = c;
+          c = fgetc(fin);
+        }
+      }
+      else if (nextchar == '/'){ // double slash comment case
+        while (c != '\n'){
+          lastchar = c;
+          c = fgetc(fin);
+        }
+        fputc('\n', fout);
+      }
+      else {
+        fputc(c, fout);
       }
     }
-    if (lastchar == '/' &&  c == '*'){
-      while (lastchar != '*' && c != '/'){
-        lastchar = c;
-        c = fgetc(fin);
-        }
-      }
-    if (lastchar == '/' &&  c == '/'){
-      while (c != '\n'){
-        lastchar = c;
-        c = fgetc(fin);
-        }
-    fputc(lastchar , fout);
-    lastchar = c;
-}
-}
+    else{
+      lastchar = c;
+      fputc(lastchar, fout);
+    }
+
+
+  }
+
+
+
+
   closefiles(2, fin, fout);  // must say number of files
 
   return 0;
